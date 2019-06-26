6dd31e498754982c2ec30721a682551a58466bd5
Smurfs的格格巫 <743289123@qq.com>
2019-06-26 15:33:49 +0800

    old

--- day 01/01 hello/hello.go
+++ day 01/01 hello/hello.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello again")
+	fmt.Println("hello world")
 }
---
+++ day 01/03 goroute/goroute.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func goroute(a int) {
+	fmt.Println(a)
+}
