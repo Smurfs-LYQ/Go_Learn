4b7575fbaf54cec5f6bbeb35b9cf8dcd9a200fb4
Smurfs的格格巫 <743289123@qq.com>
2019-06-26 08:43:28 +0800

    old

--- day 01/02 add/add.go
+++ day 01/02 add/add.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func add(a, b float64) float64 {
-	return float64(a) + float64(b)
-}
+func add(a, b float64) float64 { // func 函数名(a 数据类型, b 数据类型) 返回值数据类型 {
+	return float64(a) + float64(b) // 函数体
+} // 函数尾
 
 func main() {
 	fmt.Println(add(1, 1.1))
