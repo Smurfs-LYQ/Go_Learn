9473829f7689a25f8c7589b6c11fcf799e7012a0
Smurfs的格格巫 <743289123@qq.com>
2019-06-27 10:35:59 +0800

    old

--- day_01/05_goroute_example/main/main.go
+++ day_01/05_goroute_example/main/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"Go_Learn/day_01/05_goroute_example/groute"
+	"Go_Learn/day_01/05_goroute_example/goroute"
 	"fmt"
 )
 
 func main() {
-	add := groute.Add(1, 2)
-	fmt.Println(add)
+	// 声明一个管道
+	var pipe chan int
+	// 创建管道
+	pipe = make(chan int, 1)
+	// 并发调用
+	go goroute.Add(1, 2, pipe)
+
+	// 从管道里拿数据并赋值给res变量
+	res := <-pipe
+	fmt.Println(res)
 }
