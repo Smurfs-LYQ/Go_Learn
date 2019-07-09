2011171204b00686e78330e3b07f50e5d951ec75
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-07-09 21:28:11 +0800

    new

--- day_02/project/main/main.go
+++ day_02/project/main/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	// one "Go_Learn/day_02/project/Project_1"
-	two "Go_Learn/day_02/project/Project_2"
+	one "Go_Learn/day_02/project/Project_1"
+	// two "Go_Learn/day_02/project/Project_2"
 	// thr "Go_Learn/day_02/project/Project_3"
 )
 
 func main() {
-	// one.Project1()
-	two.Project2()
+	one.Project1()
+	// two.Project2()
 	// fmt.Println(thr.Project3(10))
 }
--- test/main.go
+++ test/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	one := int('0')
-	fmt.Println(one)
+	var i, j, k int
+	i = 153 % 10
+	j = (153 / 10) % 10
+	k = (153 / 100) % 10
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
 }
