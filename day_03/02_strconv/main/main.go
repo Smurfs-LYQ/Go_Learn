54670d828217599a0d8d221c138094ac87926436
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-07-12 23:42:33 +0800

    new

--- day_03/02_strconv/main/main.go
+++ day_03/02_strconv/main/main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-
 	// 把一个整数转换成字符串
 	fmt.Printf("%T\n", strconv.Itoa(123))
+	// 也可以用字符串格式化的方式转换数据类型
+	one := 123
+	res := fmt.Sprintf("%d\n", one)
+	fmt.Printf("%T\n", res)
 
 	// 把一个字符串转成整数
-	one, err1 := strconv.Atoi("123")
-	fmt.Printf("%T\n", one)
+	two, err1 := strconv.Atoi("123")
+	fmt.Printf("%T\n", two)
 	fmt.Println(err1)
 }
