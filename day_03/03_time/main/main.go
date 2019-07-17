b2e1910ab55de312256cc4100ef69586957a7389
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-07-17 09:29:43 +0800

    new

--- day_03/03_time/main/main.go
+++ day_03/03_time/main/main.go
@@ -5,6 +5,57 @@ import (
 	"time"
 )
 
+
+// 检测程序允许耗时
+func one() {
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("123")
+}
+
 func main() {
+	/*
+	time包的一些常亮
+		const (
+			Nanosecond Duration = 1				// 纳秒
+			Microsecond = 1000 * Nanosecond		// 微妙
+			Millisecond = 1000 * Microsecond	// 毫秒
+			Second      = 1000 * Millisecond	// 秒
+			Minute      = 60   * Second			// 分
+			Hour        = 60   * Minute			// 小时
+		)
+	*/
+
+	// 获取当前时间
 	fmt.Println(time.Now())
+
+	// 获取当前月
+	fmt.Println(time.Now().Month())
+
+	// 获取当前天
+	fmt.Println(time.Now().Day())
+
+	// 获取当前分钟
+	fmt.Println(time.Now().Minute())
+
+	// 获取当前年份
+	fmt.Println(time.Now().Year())
+
+	// 格式化
+	// Golang诞生时间:2006/02/1 15:04:
+	now := time.Now()
+
+	fmt.Println(now.Format("02/1/2006 15:04:05"))
+	fmt.Println(now.Format("2006/02/1 15:04:05"))
+	fmt.Println(now.Format("2006/02/1"))
+
+	start := time.Now().UnixNano()
+	one()
+	end := time.Now().UnixNano()
+	fmt.Println((end - start)/1000)
+
+
+	// 获取当前时间戳(秒)
+	fmt.Println(time.Now().Unix())
+	// 获取当前时间戳(纳秒)
+	fmt.Println(time.Now().UnixNano())
 }
--- day_03/04_nil/main/main.go
+++ day_03/04_nil/main/main.go
@@ -22,4 +22,12 @@ func main() {
 	// 传入变量one的内存地址(two), 通过one的内存地址来修改one的变量值
 	test(two)
 	fmt.Println(one)
+
+	// 声明变量thr为字符串指针
+	var thr *string
+	// 给thr赋值，值为one的内存地址
+	thr = &one
+	// 修改thr的值，因为它指向的时one的内存，所以one也会发生改变
+	*thr = "Hello LYQ"
+	fmt.Println(one)
 }
