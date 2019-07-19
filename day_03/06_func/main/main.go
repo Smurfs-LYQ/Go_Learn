27b4038a95205ea4e92f598fb675b61c08d9c7ad
Smurfs的格格巫 <743289123@qq.com>
2019-07-19 16:41:32 +0800

    new

--- day_03/06_func/main/main.go
+++ day_03/06_func/main/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// Project "Go_Learn/day_03/06_func/project"
+
 /*
 // No.1
 // 自定义类型 想要成为此类型的变量要求必须与其规定的格式相同
@@ -60,11 +62,68 @@ func main() {
 */
 
 // No.4 可变参数
-func add(one int) int {
-	fmt.Println("123")
-	return "123"
+/*
+// No.4_1 0-无限个参数
+func add(one ...int) int {
+	sum := 0
+	for _, v := range one {
+		sum += v
+	}
+	return sum
+}
+*/
+
+/*
+// No.4_2 最少一个参数
+func add(one int, two ...int) int {
+	var sum int = one
+	for i := 0; i < len(two); i++ {
+		sum += two[i]
+	}
+	return sum
+}
+
+// No.4_2 最少两个参数
+func add(one, two int, thr ...int) int {
+	var sum int = one
+	for i := 0; i < len(thr); i++ {
+		sum += thr[i]
+	}
+	return sum + two
 }
 
 func main() {
-	fmt.Println("test")
+	// No.4_1
+	// fmt.Println(add(10, 1, 2, 3, 4, 5))
+	// No.4_2
+	fmt.Println(add(10, 10, 1, 2, 3, 4, 5))
+}
+*/
+
+// No.5_1 defer使用示例
+func defer_test() {
+	i := 0
+	// 它返回的结果是0, 因为它在定义时, i的值是0, 他会在程序的最后执行
+	defer fmt.Println(i)
+	i++
+	fmt.Println(i)
+}
+
+// No.6 匿名函数
+func test(a, b int) {
+	res := func(a1, b1 int) int { // 匿名函数不需要写函数名
+		return (a1 + b1)
+	}(a, b) // 匿名函数直接在函数的结尾就可以直接加 () 进行调用, 也可以通过变量名"res"进行调用
+	fmt.Println(res)
+}
+
+func main() {
+	// Project_1
+	// fmt.Println(project.Project_1("h", "e", "l", "l", "o"))
+
+	// No.5_1 defer的使用示例
+	// defer_test()
+
+	// No.6 匿名函数
+	test(1, 1)
 }
