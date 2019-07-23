d46adb3a3cae7537e756918b88a128d3e1d6f771
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-07-23 21:53:18 +0800

    new

--- day_03/project/Project_05_dashu/dashu.go
+++ day_03/project/Project_05_dashu/dashu.go
@@ -5,26 +5,64 @@ import (
 	"strings"
 )
 
-func res(num1, num2 string) string {
+func res(num1, num2 string) (result string) {
 
-	one := len(num1)
-	two := len(num2)
+	index1 := len(num1) - 1
+	index2 := len(num2) - 1
+	left := 0
 
-	if one < two {
-		num1, num2 = num2, num1
-	}
-	result := []byte(num1)
-	for i := one - 1; i >= two; i-- {
-		a := int(num1[i]) - 48
-		b := int(num2[i-two]) - 48
-		he := a + b
-		if he >= 10 {
-			fmt.Println(he, " >= 10")
+	for index1 >= 0 && index2 >= 0 {
+		c1 := int(num1[index1] - '0')
+		c2 := int(num2[index2] - '0')
+
+		sum := c1 + c2 + left
+		if sum >= 10 {
+			left = 1
 		} else {
-			result[i] = byte(he + 48)
+			left = 0
 		}
+
+		c3 := sum%10 + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index1--
+		index2--
 	}
-	return string(result)
+
+	for index1 >= 0 {
+		c1 := int(num1[index1] - '0')
+
+		sum := c1 + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+
+		c3 := sum%10 + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index1--
+	}
+
+	for index2 >= 0 {
+		c2 := int(num2[index2] - '0')
+
+		sum := c2 + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+
+		c3 := sum%10 + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index2--
+	}
+
+	if left == 1 {
+		result = fmt.Sprintf("1%s", result)
+	}
+
+	return
 }
 
 // 计算两个大数相加的和，这两个大数会超过int64的表示范围
@@ -34,11 +72,8 @@ func Project_dashu() {
 	var b string
 	fmt.Scanf("%s\n", &b)
 
-	// num1 := []byte(strings.TrimSpace(a))
-	// num2 := []byte(strings.TrimSpace(b))
 	num1 := strings.TrimSpace(a)
 	num2 := strings.TrimSpace(b)
 
-	// res(num1, num2)
 	fmt.Println(res(num1, num2))
 }
