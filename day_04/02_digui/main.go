25a9395945ed48c2178ed760392dff787ab96036
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-07-28 11:19:17 +0800

    new

--- day_04/02_digui/main.go
+++ day_04/02_digui/main.go
@@ -8,7 +8,18 @@ func calc(n int) int {
 	}
 
 	// 相当于做了一个阶乘功能，每一次调用递归调用都会保留相乘之后的结果，最后返回的是相加的值
-	return calc(n-1) * n
+	res := calc(n-1) * n // 这里的*n操作乘的是calc(n-1)的返回值，也就是说*是递归执行完最后执行的
+	/*
+		calc(5-1) * 5	// 24 * 5 = 120
+			clac(4-1) * 4	// 6 * 4 = 24
+				clac(3-1) * 3	// 2 * 3 = 6
+					clac(2-1) * 2	// 1 * 2 = 2
+						if 1 == 1 {
+							return 1 // 加上后面的 *2 其实返回的是2
+						}
+	*/
+	fmt.Println(n, res)
+	return res
 }
 
 func main() {
