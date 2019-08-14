a771f54ae777531042059c8736f4a912a002dcf8
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-08-14 17:54:21 +0800

    new

--- day_04/project/Project_2/main/main.go
+++ day_04/project/Project_2/main/main.go
@@ -7,17 +7,16 @@ func main() {
 	int1 := []int{10, 4, 2, 1, 3, 8, 5}
 	var a, b int
 	for i := 0; i < len(int1); i++ {
-		a = int1[i]
-		// fmt.Printf("%d\t", a)
+		a, b = int1[i], i
+
 		for j := i + 1; j < len(int1); j++ {
 			if a > int1[j] {
 				a = int1[j]
 				b = j
 			}
 		}
-		fmt.Printf("%d##\t%d\t%d\t", b, int1[i], int1[b])
+
 		int1[i], int1[b] = int1[b], int1[i]
-		fmt.Println(int1)
 	}
 	fmt.Println(int1)
 }
