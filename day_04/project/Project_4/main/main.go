e10bb68ccbd6d00971e6f8e309e29ddfceec71ed
Smurfs的格格巫 <743289123@qq.com>
2019-08-15 17:07:24 +0800

    new

--- day_04/project/Project_4/main/main.go
+++ day_04/project/Project_4/main/main.go
@@ -2,43 +2,35 @@ package main
 
 import "fmt"
 
-func do(one []int, foot int) {
-	fmt.Println("one = ", one)
-	fmt.Println("foot = ", foot)
+func do(values []int) {
+	if len(values) <= 1 {
+		return
+	}
 
-	var top, down *int
-
-	for i := 0; i < len(one); i++ {
-		if one[i] > foot {
-			top = &one[i]
-			break
+	val, head, left, right := values[0], 0, 1, len(values)-1
+	for head < right {
+		if val < values[left] {
+			values[left], values[right] = values[right], values[left]
+			right--
+		} else {
+			values[head], values[left] = values[left], values[head]
+			head++
+			left++
 		}
 	}
 
-	for i := len(one) - 1; i >= 0; i-- {
-
-		if one[i] == *top {
-			one = append(one, foot)
-			down = &one[len(one)-1]
-			break
-		} else if one[i] < foot {
-			down = &one[i]
-			break
-		}
-	}
-
-	fmt.Println(one, *top, *down)
-	*top, *down = *down, *top
+	fmt.Println(values)
+	do(values[:head])
+	do(values[head+1:])
 }
 
 // 实现一个快速排序
 func main() {
 	int1 := []int{3, 5, 8, 1, 2, 9, 4, 7, 6}
+
+	do(int1[:])
+	fmt.Println(int1)
+
+	// Quick2Sort(int1)
 	// fmt.Println(int1)
-	for i := 0; i < 2; i++ {
-
-		do(int1[:len(int1)-1], int1[len(int1)-1])
-		fmt.Println(int1)
-	}
-
 }
--- test/main.go
+++ test/main.go
@@ -2,9 +2,31 @@ package main
 
 import "fmt"
 
+func do(values []int) {
+	if len(values) <= 1 {
+		return
+	}
+
+	val, head, left, right := values[0], 0, 1, len(values)-1
+	for head < right {
+		if val < values[left] {
+			values[left], values[right] = values[right], values[left]
+			right--
+		} else {
+			values[head], values[left] = values[left], values[head]
+			head++
+			left++
+		}
+	}
+
+	fmt.Println(values)
+	do(values[:head])
+	do(values[head+1:])
+}
+
 func main() {
-	one := []int{1, 2, 3, 4, 5}
-	fmt.Println(one)
-	one[0], one[4] = one[4], one[0]
-	fmt.Println(one)
+	int1 := []int{3, 5, 8, 1, 2, 9, 4, 7, 6}
+
+	do(int1[:])
+	fmt.Println(int1)
 }
