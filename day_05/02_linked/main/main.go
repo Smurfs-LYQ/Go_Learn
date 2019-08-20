2443353acddb8e8e8d1d32f78ed7862019404419
Smurfs的格格巫 <743289123@qq.com>
2019-08-20 17:01:26 +0800

    new

--- day_05/02_linked/main/main.go
+++ day_05/02_linked/main/main.go
@@ -11,14 +11,22 @@ func init() {
 }
 
 /*
-链表定义:
+单链表定义:
 	type T1 struct {
 		Name string
 		Next *T1 // 下一个节点的地址
 	}
 
 	1. 每个节点包含下一个节点的地址, 这样把所有的节点串起来了, 通常把链表中的第一个节点叫做链表头
-	2. 分为 单列表 和 双列表
+
+双链表定义:
+	type T1 struct {
+		Name string
+		left *T1 // 前一个节点的地址
+		right *T1 // 下一个节点的地址
+	}
+
+	1. 如果有两个指针分别指向前一个节点和后一个节点, 那就是双链表
 */
 
 // 创建结构体
@@ -38,7 +46,6 @@ func trans(head *T1) { // 形参意义为一个可供for循环持续循环的载
 
 // 尾部插入
 func install_end(end *T1) {
-	fmt.Println("尾部插入节点测试")
 	for i := 0; i < 5; i++ {
 		var test = T1{
 			Name: fmt.Sprintf("end_%d", i),
@@ -64,6 +71,21 @@ func install_top(top *T1) *T1 {
 	return top
 }
 
+// 删除指定的链
+func delLinked(links *T1, obj *T1) {
+	for links != nil {
+		if links.next == obj {
+			links.next = obj.next
+		}
+		links = links.next
+	}
+}
+
+// 在指定节点之后插入一个节点
+func addLinked(up *T1, new *T1) {
+	up.next, new.next = new, up.next
+}
+
 func main() {
 	// 创建节点 one
 	var one = T1{
@@ -86,21 +108,38 @@ func main() {
 	// 给头结点的next元素赋值
 	one.next = &two
 	two.next = &thr
-
 	// 调用打印函数
+	// trans(&one)
+
+	// fmt.Println()
+
+	/*
+		// 尾部插入节点测试
+		fmt.Println("尾部插入节点测试")
+		install_end(&thr)
+		// 调用打印函数
+		trans(&one)
+
+		fmt.Println()
+	*/
+
+	/*
+		// 头部插入节点测试
+		fmt.Println("头部插入节点测试")
+		top := install_top(&one)
+		// 调用打印函数
+		trans(top)
+
+		fmt.Println()
+	*/
+
+	// 删除一个节点
+	fmt.Println("删除一个节点")
+	delLinked(&one, &two)
 	trans(&one)
 
-	fmt.Println()
-
-	// 尾部插入节点测试
-	install_end(&thr)
-	// 调用打印函数
+	// 在指定节点之后插入一个节点
+	fmt.Println("在指定节点之后插入一个节点")
+	addLinked(&one, &two)
 	trans(&one)
-	fmt.Println()
-
-	// 头部插入节点测试
-	fmt.Println("头部插入节点测试")
-	top := install_top(&one)
-	// 调用打印函数
-	trans(top)
 }
