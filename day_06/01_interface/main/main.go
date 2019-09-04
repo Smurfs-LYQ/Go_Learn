4e2266670798007c3ca9fb5b16c8236e0460f7d2
Smurfs的格格巫 <743289123@qq.com>
2019-09-04 17:17:39 +0800

    new

--- day_06/01_interface/main/main.go
+++ day_06/01_interface/main/main.go
@@ -4,12 +4,14 @@ package main
 // "Go_Learn/day_06/01_interface/two"
 // "Go_Learn/day_06/01_interface/thr"
 // import "Go_Learn/day_06/01_interface/four"
-import "Go_Learn/day_06/01_interface/five"
+// import "Go_Learn/day_06/01_interface/five"
+import "Go_Learn/day_06/01_interface/test"
 
 func main() {
 	// one.One()
 	// two.Two()
 	// thr.Thr()
 	// four.Four()
-	five.Five()
+	// five.Five()
+	test.Test()
 }
