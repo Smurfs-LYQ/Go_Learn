3f65157f005765a2ad5829eb285fff4eaa1b4fa4
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-09-08 20:30:57 +0800

    new

--- test/main.go
+++ test/main.go
@@ -1,8 +1,22 @@
 package main
 
-func main() {
-	x, y := 1, 2
-	x, y = y+3, x+2
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-	println(x, y)
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for i := 0; i < 5; i++ {
+		input.Scan()           // 让用户进行标准输入
+		counts[input.Text()]++ // input.Text() 可以获取到刚刚用户输入的内容
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
