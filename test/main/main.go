424a987a80e953b8357e9ec5cd391f74eeb809d9
Smurfs的格格巫 <743289123@qq.com>
2019-09-29 10:45:24 +0800

    new

--- test/main/main.go
+++ test/main/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "Go_Learn/test/one"
+//import "Go_Learn/test/one"
+
+func funcMui(x,y int)(sum int,error) {
+	sum := x+y
+	return sum,nil
+}
 
 func main() {
-	one.Do()
+	//one.Do()
+	println(funcMui(1,2))
 }
