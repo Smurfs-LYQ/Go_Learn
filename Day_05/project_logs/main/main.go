968097f4435f3576b6225ea6901ed2b8294fb78d
Smurfs的格格巫 <743289123@qq.com>
2019-11-18 17:16:24 +0800

    new

--- Day_05/project_logs/main/main.go
+++ Day_05/project_logs/main/main.go
@@ -4,64 +4,68 @@ import (
 	"fmt"
 	"io/ioutil"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
 // Config 解析日志库的配置文件
 type Config struct {
-	filepath string `conf:"file_path"`
-	filename string `conf:"file_name"`
-	maxsize  int64  `conf:"max_size"`
+	Filepath string `conf:"file_path"`
+	Filename string `conf:"file_name"`
+	Maxsize  int64  `conf:"max_size"`
 }
 
 // NewConfig Config结构体的构造函数
 func NewConfig(filename string, result interface{}) {
-	/*
-		打开文件
-		一行一行读取内容，根据tag找结构体里面对应的字段
-		找到了要赋值
-
-	*/
-	// 打开文件
-	res, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Errorf("文件%s打开失败, 失败原因:%v", filename, err)
-	}
-	// 创建一个键值对map用来存放配置文件中的信息
-	var a = make(map[string]interface{})
-	for _, v := range strings.Split(string(res), "\n") {
-		r := strings.Split(v, "=")
-		a[r[0]] = r[1]
-	}
-
-	// 获取result变量的反射对象类型信息
+	// 前提条件，result必须是一个ptr
 	t := reflect.TypeOf(result)
 	v := reflect.ValueOf(result)
-	for i := 0; i < t.NumField(); i++ {
-		// 获取结构体字段的指定tag标签
-		if res, ok := a[string(t.Field(i).Tag.Get("conf"))]; ok {
-			// fmt.Println(v)
-			// fmt.Printf("%T %v\n", t.Field(i).Type, t.Field(i).Type)
-			// 获取结构体字段的类型并让他以string格式表示
-			if t.Field(i).Type.String() == "string" {
-				v.Field(i).SetString(reflect.ValueOf(res))
-			} else {
-				// fmt.Println("int")
-				v.Field(i).SetInt(reflect.ValueOf(res))
+	if t.Kind() != reflect.Ptr {
+		panic("必须传入Config对象地址\n")
+	}
+
+	tElem := t.Elem()
+	if tElem.Kind() != reflect.Struct {
+		panic("传入的Config对象必须是struct")
+	}
+	// 打开文件
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Errorf("文件打开失败, 失败原因: %v", err)
+	}
+	// 定义一个存conf的map
+	// conf_list := make(map[string]interface{})
+	// 将配置文件通过换行切割处理
+	conf := strings.Split(string(file), "\n")
+	for i := 0; i < len(conf); i++ {
+		// 将通过上面测试的conf行进行切割
+		a := strings.Split(conf[i], "=")
+		// 如果长度小于2则代表不符合规范
+		if len(a) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(a[0])
+		val := strings.TrimSpace(a[1])
+		// 利用反射给结构体赋值
+		for i := 0; i < tElem.NumField(); i++ {
+			field := tElem.Field(i)
+			if field.Tag.Get("conf") == key {
+				// 拿到每个字段的类型
+				switch field.Type.Kind() {
+				case reflect.String:
+					v.Elem().Field(i).SetString(val)
+				case reflect.Int64:
+					value64, _ := strconv.ParseInt(val, 10, 64)
+					v.Elem().Field(i).SetInt(value64)
+				}
 			}
 		}
-
 	}
-}
-
-// SetConfig 给结构体初始化值
-func SetConfig(filepath, filename string, maxsize int64) {
-
 }
 
 func main() {
 	filepath := "../init/logs.conf"
 	var c Config
-	NewConfig(filepath, c)
-
+	NewConfig(filepath, &c)
+	fmt.Println(c)
 }
