3773666ebe6a079875cdb660e425143df1d05bfa
Smurfs的格格巫 <743289123@qq.com>
2019-12-04 17:21:55 +0800

    new

--- Day_06/project/03_logs/main/main.go
+++ Day_06/project/03_logs/main/main.go
@@ -3,36 +3,50 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"reflect"
 	"strings"
 )
 
 // LogFileIni 设置日志文件信息
 type LogFileIni struct {
-	fileName string
-	filePath string
-	maxSize  int
+	fileName string `conf:"file_name"`
+	filePath string `conf:"file_path"`
+	maxSize  int64  `conf:"max_size"`
 }
 
-func getIni() {
-	// 打开文件
-	file_res, err := ioutil.ReadFile("../ini/config.ini")
-	if err != nil {
-		panic(fmt.Sprintf("文件打开失败, %v\n", err))
+// NewLogFileIni LogFileIni初始化函数
+func NewLogFileIni(iniPath string, result interface{}) {
+	// 拿到反射对象
+	t := reflect.TypeOf(result)
+	tElem := t.Elem()
+	// v := reflect.Value(result).Elem()
+
+	// 判断传入的是不是结构体指针
+	if t.Kind() != reflect.Ptr && tElem.Kind() != reflect.Struct {
+		panic("传入的必须是结构体指针类型")
 	}
-	res := strings.Split(string(file_res), "\n")
+
+	// 读取文件
+	file, err := ioutil.ReadFile(iniPath)
+	if err != nil {
+		panic(fmt.Errorf("文件打开失败, 失败原因: %v\n", err))
+	}
+
+	res := strings.Split(strings.TrimSpace(string(file)), "\n")
 	for _, v := range res {
-		value := strings.Split(v, "=")
-		// 判断返回值长度
-		if len(value) != 2 {
+		result := strings.Split(v, "=")
+		if len(result) != 2 {
 			continue
 		}
-		// 去除字符串左右的空格
-		key := strings.TrimSpace(value[0])
-		val := strings.TrimSpace(value[1])
+
+		key := strings.TrimSpace(result[0])
+		val := strings.TrimSpace(result[1])
 		fmt.Println(key, val)
 	}
 }
 
 func main() {
-	getIni()
+	iniPath := "../ini/config.ini"
+	var logConfig LogFileIni
+	NewLogFileIni(iniPath, &logConfig)
 }
--- Test/main.go
+++ Test/main.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("错误信息test")
-	fmt.Errorf("错误提示%d\n", 123)
-	fmt.Println(123)
+	panic(fmt.Errorf("错误提示%d\n", 123))
 }
