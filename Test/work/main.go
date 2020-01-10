53159a6d4c2d81238a0a66f45c32739ca07a77e6
Smurfs的格格巫 <743289123@qq.com>
2020-01-10 22:09:06 +0800

    new-1

--- Test/work/main.go
+++ Test/work/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"Go_Learn/Test/work/work"
+	"fmt"
 	"log"
 	"sync"
-	"time"
 )
 
 // names 提供了一组用来显示的名字
@@ -24,17 +24,17 @@ type namePrinter struct {
 // Task 实现 Worker 接口
 func (m *namePrinter) Task() {
 	log.Println(m.name)
-	time.Sleep(time.Second)
+	// time.Sleep(time.Second)
 }
 
 func main() {
 	// 使用两个goroutine来创建工作池
-	p := work.New(1)
+	p := work.New(5)
 
 	var wg sync.WaitGroup
 	wg.Add(100 * len(names))
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		// 迭代names切片
 		for _, name := range names {
 			// 创建一个namePrinter并提供指定的名字
@@ -47,11 +47,12 @@ func main() {
 				wg.Done()
 			}()
 		}
+		fmt.Printf("########## %d ###########", i)
 	}
 
 	wg.Wait()
+	log.Println("########## die ###########")
 
 	// 让工作池停止工作，等待所有现有的工作完成
 	p.Shutdown()
-
 }
