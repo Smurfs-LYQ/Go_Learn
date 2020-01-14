f3324d091582b73b9946c5a4019b4d4852c04608
Smurfs的格格巫 <73289123@qq.com>
2020-01-14 18:55:59 +0800

    new

---
+++ Day_11/03_Cookie_middleware/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Username string `form:"username"`
+	Password string `form:"password"`
+}
+
+func cookie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		name, err := c.Cookie("username")
+		if err != nil {
+			c.Redirect(http.StatusMovedPermanently, "/login")
+		}
+		c.Set("username", name)
+	}
+}
+
+func loginHandler(c *gin.Context) {
+	if c.Request.Method == "POST" {
+		var u User
+		err := c.ShouldBindHeader(&u)
+		if err != nil {
+			fmt.Println(err)
+			c.HTML(http.StatusOK, "login.html", gin.H{
+				"msg": "用户名和密码不能为空",
+			})
+		}
+
+		if u.Username == "smurfs" && u.Password == "123" {
+			c.SetCookie("username", u.Username, 20, "/", "127.0.0.1", false, true)
+			c.Redirect(http.StatusOK, "/user/index")
+		} else {
+			c.HTML(http.StatusOK, "login.html", gin.H{
+				"msg": "用户名或密码错误",
+			})
+		}
+	} else {
+		c.HTML(http.StatusOK, "login.html", nil)
+	}
+}
+
+func userIndexHandler(c *gin.Context) {
+	username, _ := c.Get("username")
+	c.HTML(http.StatusOK, "user.html", gin.H{
+		"username": username,
+	})
+}
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.Any("/login", loginHandler)
+	userGroup := r.Group("/user", cookie())
+	{
+		userGroup.GET("/index", userIndexHandler)
+		// userGroup.GET("/book_list")
+	}
+
+	r.Run()
+}
