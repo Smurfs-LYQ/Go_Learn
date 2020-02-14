0ab57a6ebb127b7120672984f9f7d259d969cfd9
Smurfs的格格巫 <743289123@qq.com>
2020-02-14 18:08:12 +0800

    new

--- Day_12/06_kafka/main.go
+++ Day_12/06_kafka/main.go
@@ -8,7 +8,8 @@ import (
 
 // 基于sarama第三方库开发的kafka client
 
-func send() {
+// producer 发送消息
+func producer() {
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll          // 发送完数据需要leader和follow都确认
 	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
@@ -34,6 +35,38 @@ func send() {
 	fmt.Printf("pid:%v offset:%v\n", pid, offset)
 }
 
+// consumer 接收消息
+func consumer() {
+	consumer, err := sarama.NewConsumer([]string{"127.0.0.1:9092"}, nil)
+	if err != nil {
+		fmt.Printf("fail to start consumer, err:%v\n", err)
+		return
+	}
+	partitionList, err := consumer.Partitions("test") // 根据topic取到所有的分区
+	if err != nil {
+		fmt.Printf("fail to get list of partition:err%v\n", err)
+		return
+	}
+	fmt.Println(partitonList)
+	for partition := range partitionList { // 遍历所有发的分区
+		// 针对每个分区创建一个对应的分区消费者
+		pc, err := consumer.ConsumePartition("test", int32(partition), sarama.OffsetNewest)
+		if err != nil {
+			fmt.Printf("failed to start consumer for partition %d, err:%v\n", partition, err)
+			return
+		}
+		defer pc.AsyncClose()
+		// 异步从每个分区消费信息
+		go func(sarama.PartitionConsumer) {
+			for msg := range pc.Messages() {
+				fmt.Printf("Partition: %d Offset: %d Key: %v Value:%v", msg.Partition, msg.Offset, msg.Key, msg.Value)
+			}
+		}(pc)
+	}
+}
+
 func main() {
-	send()
+	producer()
+
+	// consumer()
 }
