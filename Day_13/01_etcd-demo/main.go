a7300a73089e96eab2b1b0023d6863b3cb196410
Smurfs的格格巫 <743289123@qq.com>
2020-02-23 22:42:14 +0800

    new

---
+++ Day_13/01_etcd-demo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+)
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"192.168.1.101:2379"}, // 添加节点
+		DialTimeout: time.Second * 3,                // 设置连接超时时间
+	})
+
+	if err != nil {
+		fmt.Printf("connect to etcd faield, err: %v\n", err)
+		return
+	}
+	defer cli.Close()
+
+	fmt.Println("connect to etcd success")
+
+	// get
+	ctx, cannel := context.WithTimeout(context.Background(), time.Second)
+	resp, err := cli.Get(ctx, "smurfs")
+	cannel()
+	if err != nil {
+		fmt.Printf("get to etcd faield, err: %v\n", err)
+		return
+	}
+	for k, v := range resp.Kvs {
+		fmt.Println(k, v)
+	}
+}
---
+++ Day_13/README.md
@@ -0,0 +1,7 @@
+#### <center>Day_13</center>
+
+1. etcd 示例
+
+#### <center>笔记</center>
+
+1. > 
